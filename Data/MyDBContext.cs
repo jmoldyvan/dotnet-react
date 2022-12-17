628541a7810d20531fd81b9fcfe1fd8c0e13bb2b
jordan Moldovan <jmoldyvan@gmail.com>
2022-12-17 01:41:22 -0800

    start

---
+++ Data/MyDBContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using reactnet_tutorial.Models;
+
+namespace reactnet_tutorial.Data
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options) { }
+        public DbSet<Note> Notes { get; set; }
+    }
+}
